c31ce92bc562171d46bb954acfc02255839c491f
Oskar Schöldström <public@oxy.fi>
2017-05-30 10:35:20 -0500

    fix logo is actually icon, and add theme options

--- modules/timber-basics.php
+++ modules/timber-basics.php
@@ -16,7 +16,11 @@ class TimberBasics extends \TimberExtended {
   }
 
   public function add_timber_context($context) {
-    $context['site']->logo = new Timber\Image(get_site_icon_url());
+    $context['site']->theme_options = get_theme_mods();
+    $context['site']->icon = new Timber\Image(get_site_icon_url());
+    if ($logo_id = get_theme_mod('custom_logo')) {
+      $context['site']->logo = new Timber\Image($logo_id);
+    }
     // Timber doesn't support bedrock-like directory structures.
     $context['site']->siteurl = get_site_url();
     // Add Yoast social options if available.
